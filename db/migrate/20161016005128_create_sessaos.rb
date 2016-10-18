ac182c9199416a833bc546847d1d18b20a277520
Thiago Mayllart Macedo Silva <thiagomayllart@hotmail.com>
2016-10-18 00:45:00 -0200

    Novo campo tabela sessao

--- db/migrate/20161016005128_create_sessaos.rb
+++ db/migrate/20161016005128_create_sessaos.rb
@@ -2,6 +2,7 @@ class CreateSessaos < ActiveRecord::Migration
   def change
     create_table :sessaos do |t|
       t.date :data
+      t.string :cookie
       t.timestamps null: false
     end
   end
