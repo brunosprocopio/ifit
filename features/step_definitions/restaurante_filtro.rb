f8fbaa4b66cd2250496029ed5a3b33c955ce78b2
Thiago Mayllart Macedo Silva <thiagomayllart@hotmail.com>
2016-11-01 15:19:50 -0200

    Correção do Travis

--- features/step_definitions/restaurante_filtro.rb
+++ features/step_definitions/restaurante_filtro.rb
@@ -1,11 +1,11 @@
 Before do
-  Sessao.new :id => 1
-  Restaurante.new :id => 1, :nome => "rest1", :usuario => "teste1", :senha => "teste1", :cartao_debito => false, :cartao_credito => false, :dinheiro => true
-  Restaurante.new :id => 2, :nome => "rest2", :usuario => "teste2", :senha => "teste2", :cartao_debito => false, :cartao_credito => true, :dinheiro => true
-  Restaurante.new :id => 3, :nome => "rest3", :usuario => "teste3", :senha => "teste3", :cartao_debito => true, :cartao_credito => true, :dinheiro => false
-  RestaurantesProximo.new :id => 1, :sessao_id => 1, :restaurante_id => 1
-  RestaurantesProximo.new :id => 2, :sessao_id => 1, :restaurante_id => 2
-  RestaurantesProximo.new :id => 3, :sessao_id => 1, :restaurante_id => 3
+  Sessao.create :id => 1
+  Restaurante.create :id => 1, :nome => "rest1", :usuario => "teste1", :senha => "teste1", :cartao_debito => false, :cartao_credito => false, :dinheiro => true
+  Restaurante.create :id => 2, :nome => "rest2", :usuario => "teste2", :senha => "teste2", :cartao_debito => false, :cartao_credito => true, :dinheiro => true
+  Restaurante.create :id => 3, :nome => "rest3", :usuario => "teste3", :senha => "teste3", :cartao_debito => true, :cartao_credito => true, :dinheiro => false
+  RestaurantesProximo.create :id => 1, :sessao_id => 1, :restaurante_id => 1
+  RestaurantesProximo.create :id => 2, :sessao_id => 1, :restaurante_id => 2
+  RestaurantesProximo.create :id => 3, :sessao_id => 1, :restaurante_id => 3
 end
 
 Given(/^I visit the restaurant page$/) do
