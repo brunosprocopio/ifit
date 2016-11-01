c2bdf0b7b0ca0e0032f90972f9cc0fc84fcc2855
Thiago Mayllart Macedo Silva <thiagomayllart@hotmail.com>
2016-11-01 15:42:21 -0200

    Correção do Travis 2 - Alteração no arquivo seed.rb e travis.yml Responsável pela história welcome_index necessita corrigi-la

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def self.restaurant_distance_from_client_inkm(cep)
-    r = Restaurant.new(:id => 1, :name => "MacDonald's", :longitude => 10, :latitude => 20)
+    @r = RestaurantesProximo.new(:id => 1, :longitude => 10, :latitude => 20)
   end
 
   def self.get_location_from_cep(cep)
@@ -9,6 +9,6 @@ class WelcomeController < ApplicationController
     url = URI.parse("http://maps.google.com/maps/api/geocode/json?address=#{cep}")
     consulta = Net::HTTP.get_response(url)
     h = JSON.parse(consulta.body)
-    r = [h["results"][0]["geometry"]["location"]["lat"], h["results"][0]["geometry"]["location"]["lng"]]
+    @r = [h["results"][0]["geometry"]["location"]["lat"], h["results"][0]["geometry"]["location"]["lng"]]
   end
 end
--- db/migrate/20161016040955_create_restaurantes_proximos.rb
+++ db/migrate/20161016040955_create_restaurantes_proximos.rb
@@ -1,7 +1,8 @@
 class CreateRestaurantesProximos < ActiveRecord::Migration
   def change
     create_table :restaurantes_proximos do |t|
-      t.integer :distancia
+      t.integer :latitude
+      t.integer :longitude
       t.references :sessao, index: true, foreign_key: true
       t.references :restaurante, index: true, foreign_key: true
 
