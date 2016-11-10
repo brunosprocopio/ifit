9fb782d569f733e89368bbbe2c7510cdcb7f3f64
Bruno Procopio <brunosprocopio@gmail.com>
2016-11-09 23:38:35 -0200

    Adicionada a feature de busca de restaurantes por palavras-chave.

---
+++ features/step_definitions/busca_restaurante.rb
@@ -0,0 +1,4 @@
+Dado(/^foram encontrados os seguintes restaurantes próximos ao cliente:$/) do |restaurantes|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
